c34fdb39f4a697f17821f2cab1b2996eab117337
casey van-roose <kcvanroose@gmail.com>
2018-07-18 14:46:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+   if @roster[grade] == nil
+      @roster[grade] = []
+    end
+      @roster[grade] << student
+  end
+    
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+      new_hash = {}
+    
+      @roster.each do |key, value| 
+      new_hash[key] = value.sort
+    end
+    
+    new_hash
+  end
+
+
+end
+
+school = School.new("Bayside High School")
