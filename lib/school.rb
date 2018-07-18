6ec9a7453079140ca9d1112b3a45fcff3a762d79
Adham Muhammad <admuaq@gmail.com>
2018-07-18 19:10:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
+require 'pry'
+
 # code here!
+class School
+  
+  attr_accessor :school, :roster
+  
+  def initialize(school, roster = {})
+    @school = school
+    @roster = roster
+  end 
+  
+  def add_student(name, grade)
+    @name = name 
+    @grade = grade
+    
+    if @roster.has_key?(grade)
+    @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end 
+  end 
+ 
+  def grade(num)
+    @num = num
+    
+    t = roster.select {|names| names == num}
+
+    t.values.flatten
+  end 
+  
+  def sort
+    
+    @roster.each  do |k,v| 
+    sorted = v.sort
+    @roster[k] = sorted
+    end 
+  
+  end 
+end 
